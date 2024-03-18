7750f57cbbf3dd20cc30a7e63f4baf4aa8c1eb00
HarshVora534 <harsh200453@gmail.com>
2024-03-18 19:01:03 +0530

    Updated Tasks

--- force-app/main/default/lwc/objectSearch/objectSearch.js
+++ force-app/main/default/lwc/objectSearch/objectSearch.js
@@ -11,23 +11,27 @@ export default class ObjectSearch extends LightningElement {
     wiredObjects({ error, data }) {
         if (data) {
             this.objectOptions = Object.keys(data).map(key => ({ label: data[key], value: key }));
-        } else if (error) {
-            console.error('Error fetching object list', error);
+        } else {
+            console.error('Error fetching object list');
         }
     }
 
     handleSearchTermChange(event) {
-        this.searchTerm = event.target.value;
+        this.searchTerm = event.detail.value;
+        console.log('-->',this.searchTerm);
     }
 
     handleObjectSelection(event) {
         this.selectedObjects = event.detail.value;
+        this.searchTerm = this.selectedObjects.join(',');
     }
 
     async handleSearch() {
         try {
             const selectedObjectsString = JSON.stringify(this.selectedObjects);
+            console.log(selectedObjectsString);
             const result = await fetchSearchResults({ searchTerm: this.searchTerm, selectedObjectsString });
+            console.log('-->',result);
             this.searchResults = result;
         } catch (error) {
             console.error('Error searching:', error);
