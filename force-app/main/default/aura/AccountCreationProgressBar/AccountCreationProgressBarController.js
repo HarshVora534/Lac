df6afb1a50547003324564c7da227520f88562b1
HarshVora534 <harsh200453@gmail.com>
2024-03-27 04:06:18 +0530

    Updated Tasks

--- force-app/main/default/aura/AccountCreationProgressBar/AccountCreationProgressBarController.js
+++ force-app/main/default/aura/AccountCreationProgressBar/AccountCreationProgressBarController.js
@@ -2,61 +2,94 @@
 //     init : function(component, event, helper) {
 //         component.set("v.currentStep", 1); 
 //     },
-    
+
 //     previousStep : function(component, event, helper) {
 //         var currentStep = component.get("v.currentStep");
 //         if (currentStep > 1) {
 //             component.set("v.currentStep", currentStep - 1);
 //         }
 //     },
-    
+
 //     nextStep : function(component, event, helper) {
 //         var currentStep = component.get("v.currentStep");
 //         if (currentStep < 3) {
 //             component.set("v.currentStep", currentStep + 1);
 //         }
 //     },
-    
+
 //     createEvent : function(component, event, helper) {
-//         var eventDetails = component.get("v.eventDetails");
+//         var fieldsevent = component.get("v.fieldsevent");
+//         console.log('-->hello',fieldsevent);
+//         console.log('--->');
 //         var action = component.get("c.createEventRecord");
+//         console.log('-->get',action);
 //         action.setParams({
-//             "eventDetails": eventDetails,
+//             "fieldsevent": fieldsevent,
 //             "recordId": component.get("v.recordId")
 //         });
 //         action.setCallback(this, function(response){
-//             // handle response here
+//             var state = response.getState();
+//             console.log('-->Success');
+//             if (state === "SUCCESS") {
+//                 console.log("Event created successfully");
+//             } else if (state === "ERROR") {
+//                 var errors = response.getError();
+//                 if (errors) {
+//                     console.error("Error creating event: " + errors[0].message);
+//                 } else {
+//                     console.error("Unknown error");
+//                 }
+//             }
 //         });
 //         $A.enqueueAction(action);
 //     }
 // })
 
-
 // ({
-//     init: function(component, event, helper) {
-//         // Initialization logic here
-//         // You can perform any initialization tasks here if needed
+//     init : function(component, event, helper) {
+//         component.set("v.currentStep", 1); 
+//         component.set("v.contactId", component.get("v.recordId"));
 //     },
-//     previousStep: function(component, event, helper) {
-//         // Logic to navigate to previous step
+
+//     previousStep : function(component, event, helper) {
 //         var currentStep = component.get("v.currentStep");
 //         if (currentStep > 1) {
 //             component.set("v.currentStep", currentStep - 1);
 //         }
 //     },
-//     nextStep: function(component, event, helper) {
-//         // Logic to navigate to next step
+
+//     nextStep : function(component, event, helper) {
 //         var currentStep = component.get("v.currentStep");
 //         if (currentStep < 3) {
 //             component.set("v.currentStep", currentStep + 1);
 //         }
 //     },
-//     saveEvent: function(component, event, helper) {
-//         // Logic to save the event
-//         // You can implement the logic to save the event record here
-//         // For example, you can use lightning:recordEditForm to handle record submission
-//         var recordEditForm = component.find('recordEditForm');
-//         recordEditForm.submit();
+
+//     createEvent : function(component, event, helper) {
+//         var fieldsevent = component.get("v.fieldsevent");
+//         console.log(fieldsevent);
+//         console.log(component.get("v.recordId"));
+//         var action = component.get("c.createEventRecord");
+//         action.setParams({
+//             "fieldsevent": fieldsevent,
+//             "recordId": component.get("v.recordId")
+//         });
+//         action.setCallback(this, function(response){
+//             var state = response.getState();
+//             if (state === "SUCCESS") {
+//                 console.log("Event created successfully");
+//             } else if (state === "ERROR") {
+//                 var errors = response.getError();
+//                 if (errors) {
+//                     for (var i = 0; i < errors.length; i++) {
+//                         console.error("Error message: " + errors[i].message);
+//                     }
+//                 } else {
+//                     console.error("Unknown error");
+//                 }
+//             }
+//         });
+//         $A.enqueueAction(action);
 //     }
 // })
 
@@ -64,6 +97,7 @@
 ({
     init : function(component, event, helper) {
         component.set("v.currentStep", 1); 
+        component.set("v.contactId", component.get("v.recordId"));
     },
 
     previousStep : function(component, event, helper) {
@@ -81,19 +115,27 @@
     },
 
     createEvent : function(component, event, helper) {
+        var fieldsevent = component.get("v.fieldsevent");
+        console.log(fieldsevent);
+        console.log(component.get("v.recordId"));
         var action = component.get("c.createEventRecord");
+        action.setParams({
+            "fieldsevent": fieldsevent,
+            "recordId": component.get("v.recordId")
+        });
         action.setCallback(this, function(response){
             var state = response.getState();
-            if(state === "SUCCESS") {
-                console.log("Event created successfully.");
-            } else if(state === "ERROR") {
+            console.log(state);
+            if (state === "SUCCESS") {
+                console.log("Event created successfully");
+            } else if (state === "ERROR") {
                 var errors = response.getError();
-                if(errors) {
-                    if(errors[0] && errors[0].message) {
-                        console.error("Error message: " + errors[0].message);
+                if (errors) {
+                    for (var i = 0; i < errors.length; i++) {
+                        console.error("Error message: " + errors[i].message);
                     }
                 } else {
-                    console.error("Unknown error.");
+                    console.error("Unknown error");
                 }
             }
         });
